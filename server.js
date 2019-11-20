37b21f8536fa979b1fbb49a5b6d871b5beadc5e6
Emily Tucker <emilytucker4@yahoo.com>
2019-11-20 18:33:15 -0500

    added more stuff in the server

--- server.js
+++ server.js
@@ -9,20 +9,41 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/scrape-app", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/scrape-app", {
+    useNewUrlParser: true
+});
 
-app.get("/scrape", function(req.res){
-    axios.get("some website").then(function(response){
+app.get("/scrape", function (req, res) {
+    axios.get("some website").then(function (response) {
         var $ = cheerio.load(response.data);
 
-        $("the thing to grab").each(function(i, element){
+        $("the thing to grab").each(function (i, element) {
             var result = {};
 
-            
-        })
-    })
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+            db.Article.create(result)
+            .then(function(dbArticle){
+                console.log(dbArticle);
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+        });
+        res.send("The Scrape is Complete");
+    });
+});
+
+app.get("/articles", function(req, res){
+    
 })
