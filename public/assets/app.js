3166fda42acb0beb7c02bd15059556f76c110ab7
Emily Tucker <emilytucker4@yahoo.com>
2019-12-24 21:06:08 -0500

    finished the app.js

--- public/assets/app.js
+++ public/assets/app.js
@@ -19,6 +19,31 @@ $(document).on("click", "li", function(){
         $("#comments").append("<h3>" + data.title + "<h3>");
         $("#comments").append("<input id='titleInput' name='title'>");
         $("#comments").append("<textarea id='bodyInput' name='body'></textarea>");
-        $("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment")
+        $("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment</button>");
+
+        if (data.comment){
+            $("#titleInput").val(data.comment.title);
+            $("#bodyInput").val(data.comment.body);
+        }
+    });
+});
+
+$(document).on("click", "#saveComment", function(){
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "POST",
+        url:"/articles/" + thisId,
+        data:{
+            title: $("#titleInput").val(),
+            body: $("#bodyInput").val()
+        }
     })
+    .then(function(data){
+        console.log(data);
+
+        $("#comment").empty();
+    });
+    $("#titleInput").val("");
+    $("#bodyInput").val("");
 })
